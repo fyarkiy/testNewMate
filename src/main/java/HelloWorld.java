16b334c6d0d3cc8debf5ad8cd68b56d41fcad574
FYarkiy <flx_jb@yahoo.com>
2020-07-22 22:33:49 +0300

    HelloWorld was changed - new lines New class HelloWorld1

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
         for (int i = 1; i < 3; i++) {
-            if (i == 1) System.out.println("Hello world");
-            else System.out.println("Good night world");
+            if (i == 1) System.out.println("Hello mates");
+            else System.out.println("Good night mates");
         }
     }
 }
